f59613781bc8118ed7096af10e54a3eaca30ff48
Anton <s1211894@student.windesheim.nl>
2025-10-15 12:38:45 +0200

    Refactor GroceryListItemsRepository for database support
    - Updated `GroceryListItemsRepository` to inherit from `DatabaseConnection`.
    - Added `Microsoft.Data.Sqlite` for SQLite operations.
    - Initialized `groceryListItems` as an empty list.
    - Implemented table creation and initial data insertion in the constructor.
    - Enhanced `GetAll` method to retrieve data from the database.
    - Added a line to read an unused `clientId` column from the database.

--- Grocery.Core.Data/Repositories/GroceryListItemsRepository.cs
+++ Grocery.Core.Data/Repositories/GroceryListItemsRepository.cs
@@ -1,25 +1,52 @@
 ﻿using Grocery.Core.Interfaces.Repositories;
 using Grocery.Core.Models;
+using Microsoft.Data.Sqlite;
 
 namespace Grocery.Core.Data.Repositories
 {
-    public class GroceryListItemsRepository : IGroceryListItemsRepository
+    public class GroceryListItemsRepository : DatabaseConnection, IGroceryListItemsRepository
     {
-        private readonly List<GroceryListItem> groceryListItems;
+        private readonly List<GroceryListItem> groceryListItems = [];
 
         public GroceryListItemsRepository()
         {
-            groceryListItems = [
-                new GroceryListItem(1, 1, 1, 3),
-                new GroceryListItem(2, 1, 2, 1),
-                new GroceryListItem(3, 1, 3, 4),
-                new GroceryListItem(4, 2, 1, 2),
-                new GroceryListItem(5, 2, 2, 5),
-            ];
+
+            //ISO 8601 format: date.ToString("o", CultureInfo.InvariantCulture)
+            CreateTable(@"CREATE TABLE IF NOT EXISTS groceryListItems (
+                            [Id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                            [GroceryListID] INTERGER NOT NULL,
+                            [ProductID] DATE NOT NULL,
+                            [Amount] NVARCHAR(12) NOT NULL");
+            List<string> insertQueries =   [@"INSERT OR IGNORE INTO groceryListItems(GroceryListID, ProductID, Amount) VALUES( 1, 1, 3)",
+                                            @"INSERT OR IGNORE INTO groceryListItems(GroceryListID, ProductID, Amount) VALUES( 1, 2, 1)",
+                                            @"INSERT OR IGNORE INTO groceryListItems(GroceryListID, ProductID, Amount) VALUES( 1, 3, 4)",
+                                            @"INSERT OR IGNORE INTO groceryListItems(GroceryListID, ProductID, Amount) VALUES( 2, 1, 2)",
+                                            @"INSERT OR IGNORE INTO groceryListItems(GroceryListID, ProductID, Amount) VALUES( 2, 2, 5)"];
+            InsertMultipleWithTransaction(insertQueries);
+            GetAll();
+
         }
 
         public List<GroceryListItem> GetAll()
         {
+            groceryListItems.Clear();
+            string selectQuery = "SELECT Id, GroceryListID, ProductID, Amount FROM groceryListItems";
+            OpenConnection();
+            using (SqliteCommand command = new(selectQuery, Connection))
+            {
+                SqliteDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int ID = reader.GetInt32(0);
+                    int GroceryListID = reader.GetInt32(1);
+                    int ProductID = reader.GetInt32(2);
+                    int Amount = reader.GetInt32(3);
+                    int clientId = reader.GetInt32(4);
+                    groceryListItems.Add(new(ID, GroceryListID, ProductID, Amount));
+                }
+            }
+            CloseConnection();
             return groceryListItems;
         }
 
