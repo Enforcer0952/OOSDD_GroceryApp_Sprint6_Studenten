43847ca5c561116417dce8ec7895cb10b49ea662
Anton <s1211894@student.windesheim.nl>
2025-10-16 13:34:29 +0200

    Refactor ProductRepository for database integration
    - Updated ProductRepository to inherit from DatabaseConnection.
    - Added Microsoft.Data.Sqlite for SQLite operations.
    - Replaced private products list with a public Products property.
    - Constructor now creates the ProductRepository table if it doesn't exist.
    - Added initial data insertion using SQL queries.
    - Modified GetAll to read products from the database.
    - Updated Get and Add methods to work with the new database structure.
    - Leave Delete method unimplemented for future development.

--- Grocery.Core.Data/Repositories/ProductRepository.cs
+++ Grocery.Core.Data/Repositories/ProductRepository.cs
@@ -1,32 +1,77 @@
 ﻿using Grocery.Core.Interfaces.Repositories;
 using Grocery.Core.Models;
+using Microsoft.Data.Sqlite;
 
 namespace Grocery.Core.Data.Repositories
 {
-    public class ProductRepository : IProductRepository
+    public class ProductRepository : DatabaseConnection, IProductRepository
     {
-        private readonly List<Product> products;
+        private readonly List<Product> Products = [];
         public ProductRepository()
         {
-            products = [
-                new Product(1, "Melk", 300, new DateOnly(2025, 9, 25), 0.95m),
-                new Product(2, "Kaas", 100, new DateOnly(2025, 9, 30), 7.98m),
-                new Product(3, "Brood", 400, new DateOnly(2025, 9, 12), 2.19m),
-                new Product(4, "Cornflakes", 0, new DateOnly(2025, 12, 31), 1.48m)];
+
+            //ISO 8601 format: date.ToString("o", CultureInfo.InvariantCulture)
+            CreateTable(@"CREATE TABLE IF NOT EXISTS ProductRepository(
+                            [Id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                            [Name] STRING NOT NULL,
+                            [Stock] INTERGER NOT NULL,
+                            [ShelfLife] DATE NOT NULL,
+                            [Price] REAL NOT NULL,
+                            UNIQUE(Name, ShelfLife))");
+
+            List<string> insertQueries = [@"INSERT OR IGNORE INTO ProductRepository(Name, Stock, ShelfLife, Price) VALUES( Melk, 300, '2025-09-25', 0.95)",
+                                            @"INSERT OR IGNORE INTO ProductRepository(Name, Stock, ShelfLife, Price) VALUES( Kaas, 100, '2025-09-30', 7.95)",
+                                            @"INSERT OR IGNORE INTO ProductRepository(Name, Stock, ShelfLife, Price) VALUES( Brood, 400, '2025-09-12', 2.19)",
+                                            @"INSERT OR IGNORE INTO ProductRepository(Name, Stock, ShelfLife, Price) VALUES( Cornflakes, 0, '2025-12-31', 1.48)"];
+            InsertMultipleWithTransaction(insertQueries);
+            GetAll();
         }
+
         public List<Product> GetAll()
         {
-            return products;
+            Products.Clear();
+            string selectQuery = "SELECT Id, Name, Stock, ShelfLife, Price FROM ProductRepository";
+            OpenConnection();
+            using (SqliteCommand command = new(selectQuery, Connection))
+            {
+                SqliteDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int id = reader.GetInt32(0);
+                    string name = reader.GetString(1);
+                    int stock = reader.GetInt32(2);
+                    DateOnly shelflife = DateOnly.FromDateTime(reader.GetDateTime(3));
+                    Decimal Price = reader.GetDecimal(4);
+                    Products.Add(new(id, name, stock, shelflife, Price));
+                }
+            }
+            CloseConnection();
+            return Products;
         }
 
         public Product? Get(int id)
         {
-            return products.FirstOrDefault(p => p.Id == id);
+            return Products.FirstOrDefault(p => p.Id == id);
         }
 
         public Product Add(Product item)
         {
-            throw new NotImplementedException();
+            int recordsAffected;
+            string insertQuery = $"INSERT INTO ProductRepository(Name, Stock, ShelfLife, Price) VALUES(@Name, @Stock, @ShelfLife, @Price) Returning RowId;";
+            OpenConnection();
+            using (SqliteCommand command = new(insertQuery, Connection))
+            {
+                command.Parameters.AddWithValue("Name", item.Name);
+                command.Parameters.AddWithValue("Stock", item.Stock);
+                command.Parameters.AddWithValue("Shelflife", item.ShelfLife);
+                command.Parameters.AddWithValue("Price", item.Price);
+
+                //recordsAffected = command.ExecuteNonQuery();
+                item.Id = Convert.ToInt32(command.ExecuteScalar());
+            }
+            CloseConnection();
+            return item;
         }
 
         public Product? Delete(Product item)
