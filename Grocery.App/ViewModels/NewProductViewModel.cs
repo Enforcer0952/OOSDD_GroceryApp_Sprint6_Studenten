3d73e34189138036bb04cd1929a6777e78b78227
Anton <s1211894@student.windesheim.nl>
2025-10-16 14:38:29 +0200

    Add NewProductView and related functionality
    - Registered a new route for `NewProductView` in `AppShell.xaml.cs`.
    - Updated `Grocery.App.csproj` to include `NewProductView` files.
    - Registered `NewProductView` and `NewProductViewModel` in `MauiProgram.cs`.
    - Enhanced `ProductViewModel` with `AddProduct` command for navigation.
    - Added toolbar item in `ProductView.xaml` for adding products.
    - Modified exception handling in `GroceryListItemsRepository.cs`.
    - Created `NewProductViewModel` for managing new product addition.
    - Defined UI for `NewProductView` in `NewProductView.xaml`.
    - Set up binding context in `NewProductView.xaml.cs`.

---
+++ Grocery.App/ViewModels/NewProductViewModel.cs
@@ -0,0 +1,40 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Grocery.Core.Interfaces.Services;
+using Grocery.Core.Models;
+using Grocery.App.Views;
+using Grocery.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grocery.App.ViewModels
+{
+    public partial class NewProductViewModel : BaseViewModel
+    {
+        private readonly IProductService _productService;
+
+        Client client;
+
+        [ObservableProperty]
+        string productName;
+        public NewProductViewModel(IProductService productService, GlobalViewModel global)
+        {
+            _productService = productService;
+            client = global.Client;
+        }
+
+        [RelayCommand]
+        private async Task AddProduct()
+        {
+            if (client.Role == Role.Admin)
+            {
+                _productService.Add(new Product(0, productName, 0));
+            }
+            await Shell.Current.GoToAsync(nameof(ProductView), true);
+
+        }
+    }
+}
--- Grocery.Core.Data/Repositories/GroceryListItemsRepository.cs
+++ Grocery.Core.Data/Repositories/GroceryListItemsRepository.cs
@@ -38,7 +38,8 @@ namespace Grocery.Core.Data.Repositories
 
         public GroceryListItem? Delete(GroceryListItem item)
         {
-            throw new NotImplementedException();
+            throw new 
+                ();
         }
 
         public GroceryListItem? Get(int id)
